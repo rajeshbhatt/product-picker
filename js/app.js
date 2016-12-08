f21700dc9d1612708157d72c239126e30180a3ed
Lokesh Yadav <lyadav3@sapient.com>
2016-12-08 12:01:52 +0530

    HTML Changes restored

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-(function() {
+(function () {
     var picker = picker || {};
     picker.config = picker.config || {};
     picker.staticPath = 'D:\workspace\product-picker_1\product-picker\js'
@@ -12,17 +12,17 @@
     /******** Load jQuery if not present *********/
     if (window.jQuery === undefined || window.jQuery.fn.jquery !== '3.1.1') {
         var script_tag = document.createElement('script');
-        script_tag.setAttribute("type","text/javascript");
+        script_tag.setAttribute("type", "text/javascript");
         script_tag.setAttribute("src",
             "https://code.jquery.com/jquery-3.1.1.js");  //can be changes if we want to use diff version
         if (script_tag.readyState) {
-          script_tag.onreadystatechange = function () { // For old versions of IE
-              if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                  scriptLoadHandler();
-              }
-          };
+            script_tag.onreadystatechange = function () { // For old versions of IE
+                if (this.readyState == 'complete' || this.readyState == 'loaded') {
+                    scriptLoadHandler();
+                }
+            };
         } else {
-          script_tag.onload = scriptLoadHandler;
+            script_tag.onload = scriptLoadHandler;
         }
         // Try to find the head, otherwise default to the documentElement
         (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
@@ -44,94 +44,93 @@
 
     /******** Our main function ********/
     function main() {
-        jQuery(document).ready(function($) {
+        jQuery(document).ready(function ($) {
             var $ = $;
             picker.$ = $;
-            function loadScript(src, file, cb){
-                    var elm = document.createElement('script'),
-                        head = document.getElementsByTagName('head')[0],
-                        isLoaded = false;
-                    elm.src = src;
-                    elm.type = "application/javascript";
-                    if(cb){
-                        elm.onload = cb.bind({fName:file});
-                    }
-                    document.body.appendChild(elm);
+            function loadScript(src, file, cb) {
+                var elm = document.createElement('script'),
+                    head = document.getElementsByTagName('head')[0],
+                    isLoaded = false;
+                elm.src = src;
+                elm.type = "application/javascript";
+                if (cb) {
+                    elm.onload = cb.bind({ fName: file });
+                }
+                document.body.appendChild(elm);
             }
 
-            function loadTemplates(tmpl, cb){
+            function loadTemplates(tmpl, cb) {
 
-                    render($(tmpl));
+                render($(tmpl));
             }
 
-            function setGlobal(data){
-
-                picker.containerData = JSON.parse(data);
+            function setGlobal(data) {
+                picker.containerData = typeof data === 'string' ? JSON.parse(data) : data;
                 getData('./js/index.html', loadTemplates);
             }
 
-            function render($tmpl){
+            function render($tmpl) {
                 var $template = $tmpl.find('#product-template');
                 _.templateSettings.variable = "pp";
-                    var template = _.template(
-                        $template.html()
-                     );
-                     $tmpl.find('#product-container-main').after(
-                        template(picker.containerData)
-                     );
-                     $('#widget-ppicker').html($tmpl);
-                     loadExtFile();
+                var template = _.template(
+                    $template.html()
+                );
+                $tmpl.find('#product-container-main').after(
+                    template(picker.containerData)
+                );
+                $('#widget-ppicker').html($tmpl);
+                loadExtFile();
             }
 
-            function loadData(){
-                getData('./data.json',setGlobal);
+            function loadData() {
+                getData('./data.json', setGlobal);
 
             }
             //shift
-            function getData(src,cb){
+            function getData(src, cb) {
                 $.ajax({
-                  url: src,
-                  type: 'GET',
-                  success: function(data) {
-                    if(cb){
-                        cb(data);
+                    url: src,
+                    type: 'GET',
+                    success: function (data) {
+                        if (cb) {
+                            cb(data);
+                        }
+                    },
+                    error: function (e) {
+                        console.log(e.message);
+                        return false;
                     }
-                  },
-                  error: function(e) {
-                	console.log(e.message);
-                	return false;
-                  }
                 });
             }
-            function initializeScript(e){
+            function initializeScript(e) {
 
-                if(this.fName === 'drawer'){  //@todo wrong approach
-                    if(picker.drawer)  new picker.drawer();
-//                    loadData();
+                if (this.fName === 'drawer') {  //@todo wrong approach
+                    if (picker.drawer) new picker.drawer();
+                    //                    loadData();
                 }
-                if(this.fName === 'accordian'){
+                if (this.fName === 'accordian') {
                     if (picker.accordion) new picker.accordion();
                 }
 
             }
 
-            function loadExtFile(){
+            function loadExtFile() {
                 var jsScript = {
                     widgetCore: './core.js',
                     accordian: './module.accordion.js',
                     drawer: 'module.drawer.js'
                 };
-                for(var file in jsScript){
-                    loadScript(jsScript[file],file,initializeScript);
+                for (var file in jsScript) {
+                    loadScript(jsScript[file], file, initializeScript);
                 }
             }
-            function loadJSDependency(jsDependency){
+            function loadJSDependency(jsDependency) {
                 var jsDependency = jsDependency || {
-                            lodash : 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.2/lodash.min.js'
+                    lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.2/lodash.min.js'
 
-                        };
-                for(var file in jsDependency){
-                    loadScript(jsDependency[file],null,loadData );
+                };
+                for (var file in jsDependency) {
+                    loadScript(jsDependency[file], null, loadData);
                 }
 
             }
@@ -142,5 +141,5 @@
         });
     }
 
-    })(); // We call our anonymous function immediately
+})(); // We call our anonymous function immediately
 
